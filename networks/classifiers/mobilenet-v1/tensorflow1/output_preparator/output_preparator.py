456cb9951472e9eeee6e8fcbedbdfcfbaa3a9143
Nassim Bouberbachene <nbouberbachene@kalrayinc.com>
2024-07-04 15:24:45 +0200

    [Networks] Update classifiers output proc
    Link: https://gerrit.kalray.eu/id/I7dcb9f072fcdcf426820c06e4a99628b7830c657
    
    [networks] googlenet tensorflow1: change output_nodes_name
    
    Link: https://gerrit.kalray.eu/id/I4af797c11d3cf47bf1e9ab7f294bf188155bf774
    
    [networks] update networks sources
    
    [Network] ssd mbv1: change output preparator pb file type
    
    [networks] classifiers: encapsulate output computation
    
    Link: https://gerrit.kalray.eu/id/Ib46f0a1863ae6f77917b9342c8ffd0404cdb0032
    
    [Network] ssd mbv2: change output preparator pb file type
    
    [Network] ssd resnet34: change output preparator pb file type

--- networks/classifiers/mobilenet-v1/tensorflow1/output_preparator/output_preparator.py
+++ networks/classifiers/mobilenet-v1/tensorflow1/output_preparator/output_preparator.py
@@ -1,7 +1,8 @@
-import numpy as np
-import os
 import cv2
 
+
+head = "\x1b[0;30;42m"
+reset = "\x1b[0;0m"
 classes = None
 
 
@@ -21,9 +22,9 @@ def drawText(frame, lines, origin):
         cv2.putText(frame, line, origin, font, scale, (0, 0, 0), thick, cv2.LINE_AA)
 
 
-def softmax(x, axis=0):
-    res = np.exp(x) / np.sum(np.exp(x), axis=axis)
-    return res
+def process_nn_outputs(o):
+    o = o.squeeze()
+    return o
 
 
 def post_process(cfg, frame, nn_outputs, **kwargs):
@@ -38,17 +39,17 @@ def post_process(cfg, frame, nn_outputs, **kwargs):
     display = 3
     if classes is None:
         classes = [cl.strip() for cl in cfg ['classes'] if cl.strip() != '']
-    nb_classes = len(classes)
+    nb_classes = len (classes)
     # analyze the result
     assert len(nn_outputs) == 1
     output = list(nn_outputs.values())[0]
-    output = output.squeeze()
-    output = softmax(output)
+    output = process_nn_outputs(output)
     sorted_indices = output.argsort()
     legend = []
     # last <display> classes of the list, starting from the end
     for i in sorted_indices[nb_classes - 1:nb_classes - 1 - display:-1]:
         legend.append("{0:0.3f} - {1}".format(output[i], classes[i]))
     drawText(frame, legend, (10, 30))
-
+    if kwargs["dbg"]:
+        print(f"{head}  >> [Post-proc] prediction: {legend[0]}{reset}")
     return frame
